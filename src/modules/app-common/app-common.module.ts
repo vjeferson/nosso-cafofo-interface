6d6ebe92d934443964015451189c5b6528bddcee
Valdeci J. P. Rocha <valdeci.jeferson@avancoinfo.com.br>
2021-12-01 20:32:12 -0300

    add currency mask module

--- src/modules/app-common/app-common.module.ts
+++ src/modules/app-common/app-common.module.ts
@@ -9,7 +9,18 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { IconsModule } from '@modules/icons/icons.module';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { PipesModule } from './pipes/pipes.module';
-const thirdParty = [IconsModule, NgbModule, NgSelectModule, PipesModule];
+
+export const CustomCurrencyMaskConfig: CurrencyMaskConfig = {
+    align: 'left',
+    allowNegative: true,
+    decimal: ',',
+    precision: 2,
+    prefix: 'R$ ',
+    suffix: '',
+    thousands: '.',
+};
+
+const thirdParty = [IconsModule, NgbModule, NgSelectModule, PipesModule, CurrencyMaskModule];
 
 /* Containers */
 import * as appCommonContainers from './containers';
@@ -19,11 +30,28 @@ import * as appCommonComponents from './components';
 import * as appCommonGuards from './guards';
 /* Services */
 import * as appCommonServices from './services';
+import { CurrencyMaskConfig, CurrencyMaskModule, CURRENCY_MASK_CONFIG } from 'ng2-currency-mask';
 
 @NgModule({
-    imports: [CommonModule, RouterModule, FormsModule, ...thirdParty],
-    providers: [...appCommonServices.services, ...appCommonGuards.guards],
-    declarations: [...appCommonContainers.containers, ...appCommonComponents.components],
-    exports: [...appCommonContainers.containers, ...appCommonComponents.components, ...thirdParty],
+    imports: [
+        CommonModule,
+        RouterModule,
+        FormsModule,
+        ...thirdParty
+    ],
+    providers: [
+        ...appCommonServices.services,
+        ...appCommonGuards.guards,
+        { provide: CURRENCY_MASK_CONFIG, useValue: CustomCurrencyMaskConfig }
+    ],
+    declarations: [
+        ...appCommonContainers.containers,
+        ...appCommonComponents.components
+    ],
+    exports: [
+        ...appCommonContainers.containers,
+        ...appCommonComponents.components,
+        ...thirdParty
+    ]
 })
 export class AppCommonModule { }
