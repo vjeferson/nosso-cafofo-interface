cc87065b77e0ce42f361f23578aa80e1d1a7a721
Valdeci J. P. R <valdeci.jeferson@avancoinfo.com.br>
2022-02-02 14:43:04 -0300

    Fix enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: (window as any)._env.apiUrl,
-    debug: (window as any)._env.debug || false,
-    appIdFacebook: (window as any)._env.appIdFacebook,
-    appSecretFacebook: (window as any)._env.googleClientId,
-    googleClientId: (window as any)._env.googleClientId
+    apiUrl: 'https://nosso-cafofo-api.herokuapp.com',
+    debug: false
 };
